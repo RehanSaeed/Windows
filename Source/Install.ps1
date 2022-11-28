5cb6db485f0457da090694bd219d67748d76b1d5
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2022-11-28 10:24:58 +0000

    Update script

--- Source/Install.ps1
+++ Source/Install.ps1
@@ -11,9 +11,9 @@ Enable-WindowsOptionalFeature -Online -FeatureName "VirtualMachinePlatform" -All
 start "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip"
 
 # Terminal
-winget install --id "9N0DX20HK701" # Windows Terminal
-winget install --id "9MZ1SNWT0N5D" # PowerShell
-winget install --id "JanDeDobbeleer.OhMyPosh" --interactive --scope machine
+winget install --id "9N0DX20HK701"   # Windows Terminal
+winget install --id "9MZ1SNWT0N5D"   # PowerShell
+winget install --id "XP8K0HKJFRXGCK" # Oh My Posh
 
 # IDE
 winget install --id "Microsoft.VisualStudioCode" --interactive --scope machine
@@ -29,19 +29,28 @@ cd Windows\Source
 Copy-Item -Path "./Git/.gitconfig" -Destination "C:\Users\${env:UserName}"
 winget install --id "GitHub.cli" --interactive --scope machine
 
+# Windows Terminal
+Copy-Item -Path "./Terminal/settings.json" -Destination "C:\Users\${env:UserName}\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+
+# PowerShell Modules
+Install-Module -Name PowerShellGet -Force
+Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
+Install-Module -Name Terminal-Icons -Repository PSGallery
+Install-Module -Name DockerCompletion -Scope CurrentUser
+
+# PowerShell Profile
+$ProfileDirectory = Split-Path $PROFILE;
+Copy-Item -Path "./PowerShell/Microsoft.PowerShell_profile.ps1" -Destination $ProfileDirectory
+Copy-Item -Path "./PowerShell/oh-my-posh.json" -Destination $ProfileDirectory
+& $PROFILE; # Reload PowerShell Profile
+
 # Office
 winget install --id "9MSPC6MP8FM4" # Microsoft Whiteboard
 winget install --id "Notepad2mod.Notepad2mod" --interactive --scope machine
 
 # Utilities
 winget install --id "7zip.7zip" --interactive --scope machine
-
 winget install --id "XP89DCGQ3K6VLD" # Microsoft Power Toys
-$PowerToysBackupPath = "C:\Users\${env:UserName}\Documents\PowerToys\Backup";
-Copy-Item -Path "./PowerToys/settings.ptb" -Destination $PowerToysBackupPath
-Write-Output "Restore Power Toys backup froṁ: $PowerToysBackupPath"
-Write-Output "";
-
 winget install --id "9NJ3KMH29VGJ" # Enpass
 winget install --id "WinSCP.WinSCP" --interactive --scope machine
 winget install --id "9WZDNCRFJ3PV" # Windows Scan
@@ -75,38 +84,24 @@ winget install --id "XSplit.VCam" --interactive --scope machine
 
 # Linux
 
-winget install --id "9P9TQF7MRM4R" # Windows Subsystem for Linux Preview
-winget install --id "9NBLGGH4MSV6" # Ubuntu
+winget install --id "9P9TQF7MRM4R" # Windows Subsystem for Linux
+winget install --id "9PN20MSR04DW" # Ubuntu 22.04.1 LTS
 winget install --id "9P804CRF0395" # Alpine
 
-# Windows Terminal
-Copy-Item -Path "./Terminal/settings.json" -Destination "C:\Users\${env:UserName}\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
-
-# PowerShell Modules
-Install-Module -Name PowerShellGet -Force
-Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
-Install-Module -Name Terminal-Icons -Repository PSGallery
-Install-Module -Name DockerCompletion -Scope CurrentUser
-
-# PowerShell Profile
-$ProfileDirectory = Split-Path $PROFILE;
-Copy-Item -Path "./PowerShell/Microsoft.PowerShell_profile.ps1" -Destination $ProfileDirectory
-Copy-Item -Path "./PowerShell/oh-my-posh.json" -Destination $ProfileDirectory
-
 # Azure
-winget install --id "Microsoft.AzureCLI" --interactive --scope machine
-winget install --id "Microsoft.AzureCosmosEmulator" --interactive --scope machine
+winget install --id "Microsoft.AzureCLI" --interactive
+winget install --id "Microsoft.AzureCosmosEmulator"
 winget install --id "Microsoft.AzureDataStudio" --interactive --scope machine
-winget install --id "Microsoft.AzureStorageEmulator" --interactive --scope machine
-winget install --id "Microsoft.AzureStorageExplorer" --interactive --scope machine
+winget install --id "Microsoft.AzureStorageEmulator" --interactive
+winget install --id "Microsoft.AzureStorageExplorer" --interactive
 
 # Tools
 winget install --id "Docker.DockerDesktop" --interactive --scope machine
 winget install --id "Microsoft.PowerBI" --interactive --scope machine
-winget install --id "Pulumi.Pulumi" --interactive --scope machine
-winget install --id "Telerik.Fiddler" --interactive --scope machine
+winget install --id "Pulumi.Pulumi" --interactive
+winget install --id "Telerik.Fiddler.Everywhere" --interactive
 
 # Frameworks
 winget install --id "CoreyButler.NVMforWindows" --interactive --scope machine
-winget install --id "9PJPW5LDXLZ5" # Python
+winget install --id "9NRWMJP3717K" # Python 3.11
 winget install --id "Microsoft.dotnet" --interactive --scope machine
